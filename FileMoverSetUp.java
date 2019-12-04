7d69a88356b8dabaabb04978ed2b175af06571fd
Gerardo-cmd <56178845+Gerardo-cmd@users.noreply.github.com>
2019-12-03 21:29:01 -0500

    Update FileMoverSetUp.java
    changed "protected" to "public"

--- FileMoverSetUp.java
+++ FileMoverSetUp.java
@@ -3,13 +3,13 @@ package file_organizer_gui;
 import java.nio.file.Path;
 
 public class FileMoverSetUp {
-	protected static Path wordDocDirectory;
-	protected static Path pdfDirectory;
-	protected static Path textFileDirectory;
-	protected static Path powerpointDirectory;
-	protected static Path imageDirectory;
-	protected static Path excelDirectory;
-	protected static Path otherDirectory;
+	public static Path wordDocDirectory;
+	public static Path pdfDirectory;
+	public static Path textFileDirectory;
+	public static Path powerpointDirectory;
+	public static Path imageDirectory;
+	public static Path excelDirectory;
+	public static Path otherDirectory;
 	public void registerFileType(Path moveDirectory,String fileType) {
 		switch(fileType) {
 		case("Word"):
