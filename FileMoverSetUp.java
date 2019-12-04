5003f5fe1470269d4f11280e540aa62b29733652
JoshuaLamke <51033351+JoshuaLamke@users.noreply.github.com>
2019-12-03 20:32:37 -0500

    changed paths to static

--- FileMoverSetUp.java
+++ FileMoverSetUp.java
@@ -3,13 +3,13 @@ package file_organizer_gui;
 import java.nio.file.Path;
 
 public class FileMoverSetUp {
-	protected Path wordDocDirectory;
-	protected Path pdfDirectory;
-	protected Path textFileDirectory;
-	protected Path powerpointDirectory;
-	protected Path imageDirectory;
-	protected Path excelDirectory;
-	protected Path otherDirectory;
+	protected static Path wordDocDirectory;
+	protected static Path pdfDirectory;
+	protected static Path textFileDirectory;
+	protected static Path powerpointDirectory;
+	protected static Path imageDirectory;
+	protected static Path excelDirectory;
+	protected static Path otherDirectory;
 	public void registerFileType(Path moveDirectory,String fileType) {
 		switch(fileType) {
 		case("Word"):
